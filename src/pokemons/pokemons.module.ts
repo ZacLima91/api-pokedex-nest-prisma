3b93efe75c58535c6c50ff4e46506d37b2dab2c9
zac <jadielsonmanoel@gmail.com>
2022-10-10 20:55:09 -0300

    config: confir ORM

--- src/pokemons/pokemons.module.ts
+++ src/pokemons/pokemons.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { PokemonController } from './pokemons.controller';
 import { PokemonService } from './pokemons.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PokemonController],
   providers: [PokemonService],
 })
